156a595df2c589ca16c88c4d6fdb8934563a5c2c
CKirbs <ckggithub@gmail.com>
2023-12-18 14:43:13 +0800

    Homepage Add Group + Header

--- src/components/main/home/Home.jsx
+++ src/components/main/home/Home.jsx
@@ -1,9 +1,14 @@
 import ActionOptions from "./ActionOptions";
+import GroupAction from "./GroupAction";
 import GroupList from "./GroupList";
 import Filter from "./filter";
 export default function Home() {
   return (
     <main className="w-[95%] p-3">
+      <h2 className="text-center text-4xl font-semibold drop-shadow-lg bg-white dark:bg-neutral-800 p-2 rounded-lg">
+        Your List
+      </h2>
+      <GroupAction />
       <ActionOptions />
       <GroupList />
     </main>
